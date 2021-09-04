949a8c168c7093307c4dc421b6717b2f67fb79ed
Shehzaan-Mansuri <shehzaanmansuri1@gmail.com>
2021-09-03 20:52:41 -0700

    Flutter TabBarController

---
+++ android/app/src/main/kotlin/com/example/tabbarcontroller/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tabbarcontroller
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'TabBar Controller';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatelessWidget(),
+    );
+  }
+}
+
+const List<Tab> tabs = <Tab>[
+  Tab(text: 'Zero'),
+  Tab(text: 'One'),
+  Tab(text: 'Two'),
+];
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabs.length,
+      // The Builder widget is used to have a different BuildContext to access
+      // closest DefaultTabController.
+      child: Builder(builder: (BuildContext context) {
+        final TabController tabController = DefaultTabController.of(context)!;
+        tabController.addListener(() {
+          if (!tabController.indexIsChanging) {
+            // Your code goes here.
+            // To get index of current tab use tabController.index
+          }
+        });
+        return Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: tabs,
+            ),
+          ),
+          body: TabBarView(
+            children: tabs.map((Tab tab) {
+              return Center(
+                child: Text(
+                  '${tab.text!} Tab',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              );
+            }).toList(),
+          ),
+        );
+      }),
+    );
+  }
+}
